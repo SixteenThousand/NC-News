4cb90f9d988fdd1a2c62fa0c16e3238ab77a063d
Thomas Lindop <thomas16256@gmail.com>
2024-05-28 16:07:41 +0100

    >> add /api documentation endpoint

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const controllers = require("./api.controllers");
 const app = express();
 
 app.get("/api/topics", controllers.getTopics);
+app.get("/api", controllers.getApiHelp);
 
 
 module.exports = app;
