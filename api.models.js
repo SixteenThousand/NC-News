b671c5e21cfcec3e612da7478538b02c3b2b8c86
Thomas Lindop <thomas16256@gmail.com>
2024-05-28 14:23:51 +0100

    >> write getAllTopics

--- api.models.js
+++ api.models.js
@@ -1,7 +1,18 @@
 const db = require("./db/connection");
 
 
-function getAllTopics() {
+/**
+* Fetches a list of all the topics data from the DB.
+* @return Promise<Object[]>: an array of the topics, represented as 
+*   objects with properties:
+*     "slug": string
+*     "description": string
+*/
+async function getAllTopics() {
+  return db.query(`SELECT * FROM topics`)
+    .then(({ rows }) => {
+      return rows;
+    });
 }
 
 
